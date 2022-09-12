0de69155c9df1b5d2ac7109b3202964481049f63
tablepines <113372331+tablepines@users.noreply.github.com>
2022-09-12 10:18:11 -0600

    Add files via upload
    Level 0 exercise

---
+++ vivian.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 12 10:14:25 2022
+
+@author: vivianlaibui
+"""
+
+print("V")
+print("i")
+print("v")
+print("i")
+print("a")
+print("n")
