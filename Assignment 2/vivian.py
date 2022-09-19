f55aa071d15e2c2aa93446269259dad018fac532
tablepines <113372331+tablepines@users.noreply.github.com>
2022-09-19 11:31:30 -0600

    Add files via upload

---
+++ Assignment 2/vivian.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 12 10:14:25 2022
+
+@author: vivianlaibui
+"""
+# Print exercise
+print("V")
+print("i")
+print("v")
+print("i")
+print("a")
+print("n")
+# First name printed, letter by letter
+
+# Variable Exercises
+letter1  = "V"
+letter2 = "i"
+letter3 = "v"
+letterX = "i"
+letter5 = "a"
+letter6 = "n"
+
+print(letter2)
+print(letterX)
+
+letterX = "s"
+print(letter2)
+print(letterX)
+
+letterX = letter6
+print(letterX)
+print(letter6)
+letter6 = 'z'
+print(letterX)
+print(letter6)
