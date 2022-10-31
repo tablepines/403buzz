98530b5369d2cc6deb8efd64fdb709b7aac83fe3
tablepines <113372331+tablepines@users.noreply.github.com>
2022-10-30 18:55:08 -0600

    Add files via upload

---
+++ Assignment 5/Level3_403.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 17 09:02:26 2022
+
+@author: vivianlaibui
+"""
+#=====================
+#IMPORT MODULES
+#=====================
+#-import numpy and/or numpy functions * 
+import numpy as np
+#-import psychopy functions
+from psychopy import core, gui, visual, event 
+#-import file save functions
+import json
+#-(import other functions as necessary: os...)
+import os 
+
+
+#=====================
+#PATH SETTINGS
+#=====================
+#-define the main directory where you will keep all of your experiment files
+#-define the directory where you will save your data
+#-if you will be presenting images, define the image directory
+#-check that these directories exist
+
+os.getcwd()
+print(os.getcwd())
+main_dir = os.getcwd()
+image_dir = os.path.join(main_dir,'images')
+data_dir = os.path.join(main_dir,'data')
+
+os.path.isdir(image_dir)
+if not os.path.isdir(image_dir):
+    os.path.mkdir(image_dir)
+if not os.path.isdir(data_dir):
+    os.path.mkdir(data_dir)
+
+
+#=====================
+#COLLECT PARTICIPANT INFO
+#=====================
+#-create a dialogue box that will collect current participant number, age, gender, handedness
+#get date and time
+#-create a unique filename for the data
+
+#=====================
+#STIMULUS AND TRIAL SETTINGS
+#=====================
+#-number of trials and blocks * 
+nTrials = 10
+nBlocks = 2
+#-stimulus names (and stimulus extensions, if images) *
+cats = ['faces']*10
+imgs = ['im1.png', 'im2.png', 'im3.png', 'im4.png', 'im5.png', 'im6.png', 'im7.png', 'im8.png', 'im9.png', 'im10.png']
+#-stimulus properties like size, orientation, location, duration *
+stimSize = [200,200];
+stimDur = 1;
+stimOrien = [10];
+#-start message text *
+startMessage = "Welcome to the experiment, press any key to begin"
+
+#=====================
+#PREPARE CONDITION LISTS
+#=====================
+#-check if files to be used during the experiment (e.g., images) exist
+face_number = 1
+while True:
+    pics = ('face0%i.jpg' %face_number)
+    print(pics)
+    face_number = face_number + 1
+    if face_number == 10:
+        break
+    
+ims_in_dir = sorted(os.listdir(image_dir))
+cat_counter = 1
+for images in ims_in_dir:
+    if pics == ims_in_dir:
+        print('cat%i.jpg was found!' %cat_counter)
+        cat_counter = cat_counter +1
+    if not pics == ims_in_dir:
+        print("The image lists do not match up!")
+        
+#-create counterbalanced list of all conditions *
+catimgs = list(zip(cats,imgs))
+
+
+#=====================
+#PREPARE DATA COLLECTION LISTS
+#=====================
+#-create an empty list for correct responses (e.g., "on this trial, a response of X is correct") *
+corrResp = []; 
+corrResp = np.zeros(20);
+#-create an empty list for participant responses (e.g., "on this trial, response was a X") *
+arResp = [];
+arResp = np.zeros(20);
+#-create an empty list for response accuracy collection (e.g., "was participant correct?") *
+accResp = [];
+accResp = np.zeros(20);
+#-create an empty list for response time collection *
+response_Time = [];
+response_Time = np.zeros(20);
+#-create an empty list for recording the order of stimulus identities *
+stim_ID = [];
+stim_ID = np.zeros(20);
+#-create an empty list for recording the order of stimulus properties *
+stim_Prop = [];
+stim_Prop = np.zeros(20);
+
+#=====================
+#CREATION OF WINDOW AND STIMULI
+#=====================
+#-define the monitor settings using psychopy functions
+#-define the window (size, color, units, fullscreen mode) using psychopy functions
+#-define experiment start text unsing psychopy functions
+#-define block (start)/end text using psychopy functions
+#-define stimuli using psychopy functions
+#-create response time clock
+#-make mouse pointer invisible
+
+#=====================
+#START EXPERIMENT
+#=====================
+#-present start message text
+#-allow participant to begin experiment with button press
+
+#=====================
+#BLOCK SEQUENCE
+#=====================
+#-for loop for nBlocks *
+for thisBlock in range(nBlocks):
+    #-present block start message
+    print('Welcome to block' + thisBlock)
+    #-randomize order of trials here *
+    np.random.shuffle(catimgs)
+    #-reset response time clock here
+    
+    #=====================
+    #TRIAL SEQUENCE
+    #=====================    
+    #-for loop for nTrials *
+    for thisTrial in range(nTrials):
+        print('Trial' + str(thisTrial+1))
+        #-set stimuli and stimulus properties for the current trial
+        #-empty keypresses
+        
+        #=====================
+        #START TRIAL
+        #=====================   
+        #-draw stimulus
+        #-flip window
+        #-wait time (stimulus duration)
+        #-draw stimulus
+        #-...
+        
+        #-collect subject response for that trial
+        #-collect subject response time for that trial
+        #-collect accuracy for that trial
+        
+#======================
+# END OF EXPERIMENT
+#======================        
+#-write data to a file
+#-close window
+#-quit experiment
